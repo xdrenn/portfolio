891be7aef0be1190661016dbc03255b176531a3e
Anastasia Vilmovskaya <vilmovskaya@gmail.com>
2024-03-11 14:59:00 +0300

    Add tailwind dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import { Scroll, ScrollControls } from "@react-three/drei";
 import { Interface } from "./components/Interface";
 
 function App() {
+  const [section, setSection] = useState(0);
+
   return (
     <Canvas shadows camera={{ position: [15, 15, 10], fov: 20 }}>
       <ScrollControls pages={4} damping={0.1}>
+        <ScrollManager section={section} onSectionChange={setSection} />
         <Experience />
         <Scroll html>
           <Interface />
